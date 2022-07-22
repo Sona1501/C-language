228cfecc78b816c84899d9f42771d136d8e26892
Sona1501 <107426175+Sona1501@users.noreply.github.com>
2022-07-22 23:28:05 +0530

    Change.c

---
+++ Change.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void change(int a);
+int main()
+{
+    int b = 344;
+    printf("the value of b before change is %d\n", b);
+    change(b);
+    printf("the value of b after change is %d\n", b);
+    return 0;
+}
+void change(int a)
+{
+    a = 77;
+}
