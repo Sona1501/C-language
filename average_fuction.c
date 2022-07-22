f21dc73853e93b32536bcb01ccee11d010f7ad59
Sona1501 <107426175+Sona1501@users.noreply.github.com>
2022-07-22 23:24:52 +0530

    Average_function.c

---
+++ average_fuction.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+float average(int a, int b, int c);
+
+int main()
+{
+    int a, b, c;
+    printf("Enter the value of a\n");
+    scanf("%d", &a);
+    printf("Enter the value of b\n");
+    scanf("%d", &b);
+    printf("Enter the value of c\n");
+    scanf("%d", &c);
+    printf("the value of average is %f", average(a, b, c));
+
+    return 0;
+}
+float average(int a, int b, int c)
+{
+
+    float result;
+    result = (a + b + c) / 3;
+    return result;
+}
